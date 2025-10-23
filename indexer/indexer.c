8f740ae6da14b0d3028349ba2c697f1fb830283b
Ava Rosenbaum <Ava.d.Rosenbaum.26@dartmouth.edu>
2025-10-22 21:34:33 -0400

    step 3

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -15,6 +15,15 @@
 #include <string.h>
 #include <pageio.h>
 #include <webpage.h>
+#include <hash.h>
+#include <queue.h>
+
+#define HASH_LENGTH 100
+
+typedef struct wordcount {
+	char *word;
+	int count;
+} wordcount_t;
 
 // converts a word to lowercase
 char *NormalizeWord(const char *word){
@@ -46,6 +55,29 @@ char *NormalizeWord(const char *word){
 	return normalized;
 }
 
+void wordcount_delete(void *item){
+	if (item != NULL){
+		wordcount_t *wc = (wordcount_t *) item;
+		free(wc->word);
+		free(wc);
+	}
+}
+
+// search function
+bool searchfn (void *elementp, const void *keyp){
+	wordcount_t *wc = (wordcount_t *) elementp;
+	const char *key = (const char *) keyp;
+	return (strcmp(wc-> word, key) == 0);
+}
+
+static int total_sum = 0;
+
+// apply function to sum total counts
+void sumcounts (void *elementp){
+	wordcount_t *wc = (wordcount_t *) elementp;
+	total_sum += wc->count;
+}
+
 int main(int argc, char *argv[]){
 	if (argc != 2){
 		printf("usage: indexer <page_directory>\n");
@@ -56,31 +88,64 @@ int main(int argc, char *argv[]){
 	int pos = 0;
 	char *word = NULL;
 	char *normalized;
+	hashtable_t *index;
+	wordcount_t *existing;
+	wordcount_t *wc;
+	int total_wc = 0;
 	
 	if (page == NULL){
 		printf("Error: could not load page 1 from directory %s\n", pagedir);
 		exit(EXIT_FAILURE);
 	}
 
+	index = hopen(HASH_LENGTH);
+
+	if (index == NULL){
+		printf("Error: could not creat hashtable.\n");
+		webpage_delete(page);
+		exit(EXIT_FAILURE);
+	}
 	// extract words
-	//	printf("Original words:\n");
+	/*
+	printf("Original words:\n");
 	while ((pos = webpage_getNextWord(page, pos, &word)) > 0){
-		//	printf("%s\n", word);
+		printf("%s\n", word);
 		free(word);
 	}
-	
+	*/
 	
   pos = 0;
+	
 	// normalized words
 	while ((pos = webpage_getNextWord(page, pos, &word)) > 0){
 		normalized = NormalizeWord(word);
 		free(word);
 		if (normalized != NULL) {
-			printf("%s\n", normalized);
-			free(normalized);
+			total_wc++;
+			existing = hsearch(index, searchfn, normalized, strlen(normalized));
+
+			if (existing != NULL){
+				existing->count++;
+				printf("Repeat: %s\n", normalized);
+				free(normalized);
+ 			}
+			else {
+				wc = malloc(sizeof(wordcount_t));
+				printf("%s\n", normalized);
+				wc->word = normalized;
+				wc->count = 1;
+				hput(index, wc, wc->word, strlen(wc->word));
+			}
 		}
 	}
-	
+
+	happly(index, sumcounts);
+
+	printf("Total normalized words: %d\n", total_wc);
+	printf("Sum of counts in hash table: %d\n", total_sum);
+
+	happly(index, wordcount_delete);
+	hclose(index);
 	webpage_delete(page);
 					 
 }
