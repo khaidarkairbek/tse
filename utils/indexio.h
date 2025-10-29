0a92bfdd6b9487dc5393d20decb6ce052544b093
Khaidar Kairbek <khaidarkairbek@proton.me>
2025-10-29 13:05:40 -0400

    add indexio utility

---
+++ utils/indexio.h
@@ -0,0 +1,31 @@
+#pragma once
+/* 
+ * indexio.h --- 
+ * 
+ * Author: Ava D. Rosenbaum, Khaidar Kairbek
+ * Created: 10-28-2025
+ * Version: 1.0
+ * 
+ * Description: 
+ * 
+ */
+
+#include <stdio.h>
+#include <hash.h>
+
+
+/*
+ * indexsave -- writes your in-memory hashtable to a file
+ *  returns 0 if successfully saved to file
+ *  returns -1 if error
+ */
+int32_t indexsave(hashtable_t *htp, char *indexnm);
+
+
+
+/*
+ * indexload -- reads file and reconstructs the hashtable in memory
+ * returns NULL if unsuccessful
+ * returns hashtable if successful
+ */
+hashtable_t *indexload(char *indexnm);
