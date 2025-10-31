80e91e8854b2357a4ff8eb4fc07331c9ffed4bb2
Ava Rosenbaum <Ava.d.Rosenbaum.26@dartmouth.edu>
2025-10-31 10:50:08 -0400

    step2

--- querier/query.c
+++ querier/query.c
@@ -11,12 +11,18 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <hash.h>
+#include <queue.h>
+#include <indexio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <ctype.h>
 
 static uint64_t LINE_LEN = 256;
+#define INDEX_FILE "indexfile"
 
 // converts a word to lowercase
 char *NormalizeWord(const char *word){
@@ -36,6 +42,34 @@ char *NormalizeWord(const char *word){
 	return normalized;
 }
 
+// hsearch helper for document_t
+static bool match_docid(void *elementp, const void *keyp){
+	document_t *doc = (document_t *) elementp;
+	uint64_t target = *(uint64_t *) keyp;
+	return(doc->id == target);
+}
+
+// hsearch helper for word_index_t
+static bool match_word(void *elementp, const void *keyp){
+	word_index_t *w = (word_index_t *) elementp;
+	return strcmp(w->word, (const char *)keyp) == 0;
+}
+
+// get count for a given word in document ID 1
+static int count_for_word(hashtable_t *index, const char *word){
+	if (!index || !word) return 0;
+
+	// find word_index struct in hashtable
+	word_index_t *entry = hsearch(index, match_word, word, strlen(word));
+	if (entry == NULL || entry->docs == NULL) return 0;
+
+	uint64_t docid = 1;
+	document_t *doc = qsearch(entry->docs, match_docid, &docid);
+	if (doc == NULL) return 0;
+
+	return doc->count;
+}
+
 int main(void) {
 	char line[LINE_LEN];
 	char **wordList = NULL; // growing list of words
@@ -44,7 +78,15 @@ int main(void) {
 	char *lowercase;
 	int wordCount = 0;
 	int invalid = 0;
+	int count;
+	int minCount;
 
+	hashtable_t *index = indexload(INDEX_FILE);
+	if (index == NULL) {
+		printf("Error: count not load index file '%s'\n", INDEX_FILE);
+		exit(EXIT_FAILURE);
+	}
+	
 	printf("> ");
 	while (fgets(line, sizeof(line), stdin) != NULL){
 		line[strcspn(line, "\n")] = '\0'; // remove trailing newline
@@ -55,7 +97,13 @@ int main(void) {
 		}
 
 		token = strtok(line, " ");
+		
 		while (token != NULL) {
+			if (strlen(token) < 3 || strcmp(token, "and")  == 0 || strcmp(token, "or") == 0) {
+				token = strtok(NULL, " ");
+				continue;
+			}
+			
 			lowercase =  NormalizeWord(token);
 			if (lowercase == NULL){
 				invalid = 1;
@@ -76,15 +124,19 @@ int main(void) {
 			token = strtok(NULL, " ");
 		}
 		
-		if (invalid){
+		if (invalid || wordCount == 0){
 			printf("[invalid query]\n");
 		}
 		else if (wordCount > 0){
+			minCount = count_for_word(index, wordList[i]);
 			for (int i = 0; i < wordCount; i++){
-				printf("%s ", wordList[i]);
+				count = count_for_word(index, wordList[i]);
+				printf("%s:%d ", wordList[i], count);
+				if (count < minCount) minCount = count;
+
 				free(wordList[i]);
 			}
-			printf("\n");
+			printf("-- %d\n", minCount);			 
 		}
 		free(wordList);
 		wordList = NULL;
@@ -92,5 +144,8 @@ int main(void) {
 		invalid = 0;
 		printf("> ");
 	}
+	
+	hclose(index);
+	
 	return 0;
 }
